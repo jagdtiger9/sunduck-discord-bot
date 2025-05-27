ae29071b5ff48e1abfb4208e77a60a36d01718ee
cucaracha <cucaracha@mail.ru>
2025-05-27 14:34:02 +0300

    composer, run container as root to have perms to create user, setuser in the entrypoint script

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 echo "uid-gid: $GID ${GID}"
 getent group ${GID} || (groupadd --gid ${GID} g${GID} && useradd --shell /bin/bash --uid ${UID} --gid ${GID} -m u${UID})
 
-exec "$@"
+exec /sbin/setuser u${UID} "$@"
